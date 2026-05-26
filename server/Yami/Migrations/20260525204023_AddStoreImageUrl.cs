b92594ec56855f384ff188a4e8d7b831a97ad4d4
ShiraDeveloper <shiradeveloper@gmail.com>
2026-05-26 12:25:51 +0300

    add images

---
+++ server/Yami/Migrations/20260525204023_AddStoreImageUrl.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yami.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoreImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Stores",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Stores");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Menus");
+        }
+    }
+}
